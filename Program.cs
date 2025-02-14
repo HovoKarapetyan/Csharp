f9b53b4d3fe2f574675b710b63df837bbf286768
Hovo <96445695+HovoKarapetyan@users.noreply.github.com>
2025-02-14 21:39:28 +0400

    Day2 - Task1

--- Program.cs
+++ Program.cs
@@ -4,23 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day2_Task1
+namespace Day2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the number: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-
-            int tmp = 0;
-
-            while (num > 0) 
-            {
-                tmp = tmp * 10 + num % 10;
-                num /= 10;
-            }
-            Console.WriteLine($"{tmp}");
         }
     }
 }
