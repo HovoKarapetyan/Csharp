eddea861baa89b6d55e10fdb9c2ddbd55225e31e
Hovo <96445695+HovoKarapetyan@users.noreply.github.com>
2025-02-14 21:20:06 +0400

    Day2 - Task2

--- Program.cs
+++ Program.cs
@@ -10,18 +10,17 @@ namespace Day2_Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the number for Fibonacci");
-            int num1= Convert.ToInt32(Console.ReadLine());
-            int fib1 = 0;
-            int fib2 = 1;
-            int sum = fib1+ fib2;
-            Console.WriteLine($"{ fib1}\n{ fib2}");
-            for (int i = 0; i < num1; i++) {
-                Console.WriteLine($"{sum}");
-                fib1 = fib2;
-                fib2 = sum;
-                sum = fib1+ fib2;
+            Console.WriteLine("Please enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int tmp = 0;
+
+            while (num > 0) 
+            {
+                tmp = tmp * 10 + num % 10;
+                num /= 10;
             }
+            Console.WriteLine($"{tmp}");
         }
     }
 }
